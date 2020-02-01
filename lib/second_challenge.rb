e803befb69b051a41cdd2555ac73064b64150d2d
james Johnson <2013matthew2013@gmail.com>
2020-02-01 18:11:57 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,17 +3,17 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :montague => {
-      :patriarch => {53},
-      :matriarch => {54},
-      :hero => {15},
-      :hero_friends => [17,18]
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       }, 
     :capulet => {
       :montague => {
-      :patriarch => {53},
-      :matriarch => {54},
-      :hero => {15},
-      :hero_friends => [30,40]
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       }
       
     }
